7eab5a102e24e2957f7098f049e9fb410c30a607
Haruka08 <haruka.hashimoto08@gmail.com>
2022-12-05 18:56:18 +0000

    deploying 5

--- server/config/connection.js
+++ server/config/connection.js
@@ -8,7 +8,7 @@ const mongoose = require('mongoose');
 // });
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/your-database-name',
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/bookipedia',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
